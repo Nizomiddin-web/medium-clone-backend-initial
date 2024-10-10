7146eec10113fddc1dfbe9374aa891465ef8c19b
Nizomiddin <reactspringgood@gmail.com>
2024-10-10 11:53:50 +0500

    add search in article Topic and article

--- articles/filters.py
+++ articles/filters.py
@@ -4,12 +4,13 @@ from articles.models import Article
 
 
 class ArticleFilter(filters.FilterSet):
+    search = filters.CharFilter(field_name="title", method="search_filter_method")
     get_top_articles = filters.NumberFilter(field_name="views_count", method="filter_top_articles")
     topic_id = filters.NumberFilter(field_name="topics", method="filter_topic_id")
 
     class Meta:
         model = Article
-        fields = ['get_top_articles', 'topic_id', 'is_recommend']
+        fields = ['get_top_articles', 'topic_id', 'is_recommend', 'search']
 
     def filter_top_articles(self, queryset, name, value):
         if value:
@@ -19,4 +20,16 @@ class ArticleFilter(filters.FilterSet):
     def filter_topic_id(self, queryset, name, value):
         if value:
             return queryset.filter(topics__id=value)
+        return queryset
+
+    def search_filter_method(self, queryset, name, value):
+        if value:
+            queryset1 = queryset.filter(title__icontains=value)
+            if len(queryset1) == 0:
+                queryset1 = queryset.filter(topics__name__icontains=value)
+            if len(queryset1) == 0:
+                queryset1 = queryset.filter(summary__icontains=value)
+            if len(queryset1) == 0:
+                queryset1 = queryset.filter(content__icontains=value)
+            return queryset1
         return queryset
