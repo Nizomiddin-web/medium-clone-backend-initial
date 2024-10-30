817e845dc3e52813145da0b9085886e7d0777b7e
Nizomiddin <reactspringgood@gmail.com>
2024-10-30 16:13:44 +0500

    some change

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = config('SECRET_KEY', default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['medium-clone-backend-initial-tan.vercel.app','localhost']
 
 # Application definition
 DJANGO_APPS = [
